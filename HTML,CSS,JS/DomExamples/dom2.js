5d03ee4827eb80c61a2ab64b703dd758d78bf337
Sankar <sankar.yalamandla@tigeranalytics.com>
2023-02-01 19:28:27 +0530

    creating elements

--- HTML,CSS,JS/DomExamples/dom2.js
+++ HTML,CSS,JS/DomExamples/dom2.js
@@ -18,10 +18,38 @@ const changeToYellow = (event) => {
 // event deligation
 
 const changetonormalcolor = (event) => {
-   console.log(event.target);
+  //  console.log(event.target);
    console.log(event.target.getAttribute("id"))
 
   if (event.target.getAttribute("id") !== "container1") {
     document.getElementById("container1").style.background = "blue";
   }
+
 };
+
+
+const submitdata = (event) =>{
+
+  const inputValue = document.getElementById('15')
+ 
+
+  console.log(event.target);
+  console.log(document.getElementById('15'))
+  console.log(document.getElementById('15').getAttribute('id'));
+  console.log(inputValue.value)   // prints values inside input
+
+  // step 1          creating <li></li>
+  const liElement = document.createElement('li')
+  // step 2         inserting input box values inside li element <li>Mukesh</li>
+  liElement.textContent = inputValue.value
+  console.log(liElement)
+
+  // step3  appending into the ui
+  document.getElementById('ulContainer').appendChild(liElement)
+
+
+
+
+  inputValue.value  = ''          // empty the input value
+}
+
