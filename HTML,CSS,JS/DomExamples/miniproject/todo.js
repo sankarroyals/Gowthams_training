9f9539dea034bfaa6aa63caa303c27ec3185edd7
Sankar <sankar.yalamandla@tigeranalytics.com>
2023-02-08 19:38:18 +0530

    delete action

--- HTML,CSS,JS/DomExamples/miniproject/todo.js
+++ HTML,CSS,JS/DomExamples/miniproject/todo.js
@@ -1,19 +1,65 @@
-const titleHead = document.getElementsByClassName('titleinput')[0]
-const listItemValue  =  document.getElementsByClassName('todoListItems')[0]
+const titleHead = document.getElementsByClassName("titleinput")[0];
+const listItemValue = document.getElementsByClassName("todoListItems")[0];
 
-const addingList = (event) =>{
+let listItem = [];
+
+const addingList = (event) => {
+      //  it is used for id
+  let newDate = new Date()
+  
+
+  listItem.push(listItemValue.value);
+  
+//   Emptying the ul container before adding list due to avoid duplicates
+  document.getElementsByClassName("ulContainer")[0].innerHTML = ""
 
 
-const liElement =  document.createElement('li')
+  
 
-liElement.textContent =  listItemValue.value
+  for(let i=0;i<listItem.length;i++){
 
-const deleteIcon = document.createElement('i')
-deleteIcon.setAttribute('class','fa-solid fa-trash')
-liElement.appendChild(deleteIcon)
+    createlist(listItem[i],i);
+  }
+  
+};
 
-document.getElementsByClassName('ulContainer')[0].appendChild(liElement)
+const createlist = (listItem,id) => {
 
-listItemValue.value = ""
-}
+  const liElement = document.createElement("li");
+  liElement.textContent = listItem;
+  liElement.setAttribute("id",id)
 
+
+//    creating and appending deleteIcons into list
+  const deleteIcon = document.createElement("i");
+  deleteIcon.setAttribute("class", "fa-solid fa-trash")
+  deleteIcon.setAttribute("onclick","deleteData(event)")
+  liElement.appendChild(deleteIcon);
+
+
+
+  document.getElementsByClassName("ulContainer")[0].appendChild(liElement);
+
+  listItemValue.value = "";
+};
+
+
+
+const deleteData = (event) => {
+
+    const presentId = event.target.parentElement.getAttribute('id')
+    
+    listItem = listItem.filter((li,index)=>{return index != presentId})
+
+    document.getElementsByClassName("ulContainer")[0].innerHTML = ""
+    for(let i=0;i<listItem.length;i++){
+  
+      createlist(listItem[i],i);
+    }
+
+    
+    console.log(listItem);
+    
+ 
+ 
+};
